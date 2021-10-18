0bddc8c66f02cd1ce60ada6f61266299cede10c5
yashensha <m.yashensha@gmail.com>
2021-10-18 12:36:23 +0530

    js-btn-litsener-added

---
+++ script/script.js
@@ -0,0 +1,64 @@
+function getHistory()
+{
+            document.getElementById("history").innerHTML = "43"
+         }
+
+         function allZero()
+         {
+            document.getElementById("history").innerHTML = "0"
+         }
+
+            // All-numbers-event-listsening
+         function one()
+         {
+
+         }
+
+         function two()
+         {
+
+         }
+
+         function three()
+         {
+
+         }
+
+         function four()
+         {
+
+         }
+         
+         function five()
+         {
+
+         }
+         
+         function six()
+         {
+
+         }
+
+         function seven()
+         {
+
+         }
+
+         function eight()
+         {
+
+         }
+
+         function nine()
+         {
+
+         }
+
+         function zero()
+         {
+            
+        }
+
+         function duobleZero()
+         {
+         }
